bc2a7be2f98edc04179c5af7112a2ed7629639f1
Saradha Venkatachalapathy <saradhavpathy@gmail.com>
2021-08-12 16:27:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='breast_cancer_score',
     version='0.1',
-    packages=['src','src.utlis','src.nuclear_features','src.spatial_features '],
+    packages=['src','src.utlis','src.nuclear_features','src.spatial_features'],
     url='https://github.com/GVS-Lab/genomic-scoring-breast-cancer-progression.git',
     license='MIT',
     author='Saradha Venkatachalapathy',
