cdb022ed315dfe0f77b9e92406684f65c19e4fc8
Saradha Venkatachalapathy <saradhavpathy@gmail.com>
2021-08-12 14:58:08 +0200

    Add files via upload

---
+++ src/utlis/score_cells.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+# import libraries
+import sys
+sys.path.append("..")
+
+from pathlib import Path
+from glob import glob
+import pandas as pd
+import os
+from tqdm.notebook import tqdm
+import subprocess
+
+from src.utlis.segmentation_stardist_model import segment_objects_stardist2d
+from src.utlis.Run_nuclear_feature_extraction import run_nuclear_chromatin_feat_ext
+from src.utlis.visualize_scores import vis_score,color_code_MGS_image,plot_nuc_mgs_biomarker
+
+
+def score_vis_with_groundtruth_data(DNA_labeled_regions,biomarker_labeled_region,
+                                    path_to_Rscript = False,
+                                    path_to_pretrained_datamodels = False, 
+                                    path_to_seg_model = False, 
+                                    path_to_output_segmented_nuclei = False, 
+                                    path_to_output_nuclear_features = False, 
+                                    path_to_output_MGS_file = False, 
+                                    path_to_output_MGS_img = False):
+    """ Score and visualise nuclei along with ground truth(biomarker data
+    """
+    if(not path_to_Rscript):
+        path_to_Rscript = os.path.join(os.path.dirname(os.path.dirname(DNA_labeled_regions)) ,"scoring_model/")
+        
+    if(not path_to_pretrained_datamodels):
+        path_to_pretrained_datamodels = os.path.join(os.path.dirname(os.path.dirname(DNA_labeled_regions)) ,"scoring_model/model/")
+        
+    if(not path_to_seg_model):
+        path_to_seg_model = os.path.join(os.path.dirname(os.getcwd()),'models/')
+        
+    if(not path_to_output_segmented_nuclei):
+        path_to_output_segmented_nuclei = os.path.join(os.path.dirname(os.path.dirname(DNA_labeled_regions)) , "Segmented_nucleus/")
+        
+    if(not path_to_output_nuclear_features):
+        path_to_output_nuclear_features = os.path.join(os.path.dirname(os.path.dirname(DNA_labeled_regions))  , "NMCO_features/")
+        
+    if(not path_to_output_MGS_file):
+        path_to_output_MGS_file = os.path.join(os.path.dirname(os.path.dirname(DNA_labeled_regions)), "MGS/")
+        
+    if(not path_to_output_MGS_img):
+        path_to_output_MGS_img = os.path.join(os.path.dirname(os.path.dirname(DNA_labeled_regions)) , "MGS_img/")
+        
+        
+    #create output directories if they do not exist
+    Path(path_to_output_segmented_nuclei).mkdir(parents=True, exist_ok=True)
+    Path(path_to_output_nuclear_features).mkdir(parents=True, exist_ok=True)
+    Path(path_to_output_MGS_file).mkdir(parents=True, exist_ok=True)
+    Path(path_to_output_MGS_img).mkdir(parents=True, exist_ok=True)
+
+    #Segment nuclei based on a pretrained model
+    
+    print("Segmenting images.....")
+    segment_objects_stardist2d(image_dir = DNA_labeled_regions,
+                               output_dir_labels = path_to_output_segmented_nuclei,
+                               output_dir_ijroi = False,
+                               use_pretrained = False,
+                               model_name ='tissue_nuclear_segmentation',
+                               model_dir = path_to_seg_model
+                               )
+    ### Extract single nuclear features for all images 
+    print("Computing nuclear and chromatin features....")
+    dna_image_path = sorted(glob(DNA_labeled_regions + "/*tif"))
+    seg_image_path = sorted(glob(path_to_output_segmented_nuclei + "*.tif"))
+    for i in tqdm(range(len(dna_image_path))):
+        run_nuclear_chromatin_feat_ext(dna_image_path[i], seg_image_path[i], path_to_output_nuclear_features)
+    
+
+
+    # Score the nuclei based on a pretrained model (this is run in R)
+    print("Computing cell heath score (MGS)....")
+    scriptpath=os.path.join(path_to_Rscript,"score_nuclei.R")
+    subprocess.call(['Rscript',scriptpath, path_to_output_nuclear_features,path_to_pretrained_datamodels, path_to_output_MGS_file])
+    print(scriptpath)
+    
+    # Obtain plots of MGS for each image
+    tumogenesis_score=pd.read_csv(path_to_output_MGS_file+'MGS_for_plotting.csv') # read in the file
+    grouped = tumogenesis_score.groupby(tumogenesis_score.Image) #group
+    for i in tqdm(range(len(seg_image_path))): 
+        image = seg_image_path[i].rsplit('/', 1)[-1][:-4]
+        vis_score(image,grouped,seg_image_path[i],path_to_output_MGS_img,0.35,0.6,5,False)
+    
+    #Visualise the scores with biomarker/groundtruth data for all images in a folder
+    path_to_output_MGS_imgs = sorted(glob(path_to_output_MGS_img + "/*.png"))
+    path_to_biomarker_imgs = sorted(glob(biomarker_labeled_region + "/*.tif"))
+
+    for ind in range(len(dna_image_path)):
+        plot_nuc_mgs_biomarker(dna_image_path[ind], path_to_output_MGS_imgs[ind], path_to_biomarker_imgs[ind])
--- src/utlis/visualize_scores.py
+++ src/utlis/visualize_scores.py
@@ -6,13 +6,17 @@ from PIL import Image
 import numpy as np
 from tqdm.notebook import tqdm
 
-def vis_score(T_id,data,img_path,output_path,display_not_save=True):
+def vis_score(T_id,data,img_path,output_path,min_display_val = 0.1, max_display_val = 0.7,pt_size=0.5, display_not_save=True):
     """Function to visualize the MGS of a given image
        
        Parameters:
            T_id: Tissue id/Name of the image
            data: grouped dataframe that contains the centroid and MGS of nuclei 
            img_path: path to the images
+           output_path: path to store images
+           min_display_val: miminum value to be plotted
+           max_display_val: maximum value to be plotted
+           display_not_save: display plot dont save
     """
     #img = imread(img_path+T_id+'.tif')
     img = imread(img_path)
@@ -25,8 +29,8 @@ def vis_score(T_id,data,img_path,output_path,display_not_save=True):
     
     #code the tumogenesis score
     img2=ax0.scatter(score['centroid-1'], score['centroid-0'], c=(score['MGS']),
-                     s=0.5,cmap='RdYlBu',
-                    vmin=0.1, vmax=0.7)
+                     s=pt_size,cmap='RdYlBu',
+                    vmin=min_display_val, vmax=max_display_val)
     ax0.set_xlim(0,img.shape[0])
     ax0.set_ylim(img.shape[1],0)
     ax0.axis('off')
@@ -62,4 +66,36 @@ def color_code_MGS_image(T_id,data,img_path,output_path):
     im = Image.fromarray(t)
     im.save(output_path+"/"+T_id+ ".tif")
     
+def plot_nuc_mgs_biomarker(path_to_DNA_image,path_to_MGS_image,path_to_biomarker_image):
+    """Function for visualising score with DNA and biomarker labels
+    """
+    #Read in Images
+    raw_image = imread(path_to_DNA_image)
+    scored_image = plt.imread(path_to_MGS_image)
+    biomarker = plt.imread(path_to_biomarker_image)
     
+    # normalize images
+    raw_image = ((raw_image-np.min(raw_image))/(np.max(raw_image)-np.min(raw_image)))*255
+    raw_image = raw_image.astype(int)
+    biomarker = ((biomarker-np.min(biomarker))/(np.max(biomarker)-np.min(biomarker)))*255
+    biomarker = biomarker.astype(int)
+    #Visulaise the data
+
+    #save plots to show clusters
+    fig = plt.figure(figsize=(12, 4))
+    ax0 = fig.add_subplot(131)
+    ax1 = fig.add_subplot(132)
+    ax2 = fig.add_subplot(133)
+    #show raw image 
+    ax0.imshow(raw_image,aspect='auto',cmap='inferno') 
+    ax0.axis('off')
+    ax0.title.set_text('Image')
+    #show segmented image
+    ax1.imshow(scored_image,aspect='auto',cmap='Spectral') 
+    ax1.axis('off')
+    ax1.title.set_text('Nuclear health score')
+    #show segmented image
+    ax2.imshow(biomarker,aspect='auto',cmap='Greens_r') 
+    ax2.axis('off')
+    ax2.title.set_text('Biomarker')
+    plt.show()
